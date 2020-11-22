1a598f493fbf8ec5dd6b4760aa97f432068d874b
Adkham Badalbaev <monterey-bay@mail.ru>
2020-11-23 00:37:36 +0300

    fix full__card opening

--- cardInformation.js
+++ cardInformation.js
@@ -3,7 +3,7 @@ import { travels } from './travels';
 import { createNewElement } from './createNewElement';
 
 const createFullCard = (cityName, countryName, description, imagePath, shiftValue, order) => createNewElement(`
-    <div style="left: ${shiftValue}px" class="full-card ${(order > 3 && order < 6) || order > 9 ? "full-card_order_reverse" : ''} js-full-card">
+    <div style="left: ${shiftValue}px" class="full-card ${order ? "full-card_order_reverse" : ''} js-full-card">
         <img class="full-card__picture" src="${imagePath}" alt="The city you chose">
         <p class="current__city disposition_absolute">${cityName}</p>
         <p class="current__country disposition_absolute">${countryName}</p>
@@ -18,6 +18,8 @@ const createFullCard = (cityName, countryName, description, imagePath, shiftValu
 `);
 
 const swiperWrapper = document.querySelector('.swiper-wrapper');
+const swiperContainerRightBorder = swiperWrapper.parentElement.getBoundingClientRect().right;
+const FULL_CARD_WIDTH = 570;
 
 const deleteLastFullCard = [];
 let previousFullCard;
@@ -34,16 +36,26 @@ const initSlidesEventListeners = () => {
 
             let cityCode = countryCardContainer.dataset.infoAbtCity;
             let order = countryCardContainer.dataset.order;
-            console.log((order > 2 && order > 8) && order < 6);
-            console.log(order);
 
             const travel = travels[cityCode];
         
-            console.log(travel);
             const imagePath = countryCardContainer.querySelector('img').src;
-            const fullCountryCard = createFullCard(travel.city, travel.country, travel.information, imagePath, shiftValue, order);
+            
+            console.log(event.currentTarget.getBoundingClientRect().left + FULL_CARD_WIDTH, swiperContainerRightBorder);
+            const expectedRightBorder = event.currentTarget.getBoundingClientRect().left + FULL_CARD_WIDTH;
+            order = expectedRightBorder > swiperContainerRightBorder;
+            console.log(order)
+
+            const fullCountryCard = createFullCard(
+                travel.city, 
+                travel.country, 
+                travel.information, 
+                imagePath, 
+                shiftValue, 
+                order
+            );
             countryCardContainer.append(fullCountryCard);
-            console.log(fullCountryCard, countryCardContainer)
+            // console.log(fullCountryCard, countryCardContainer)
             previousFullCard = fullCountryCard;
 
             const icon = fullCountryCard.querySelector(".icon");
